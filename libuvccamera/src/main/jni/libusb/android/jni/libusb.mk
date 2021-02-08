83b1e14c0b48820a3d5ce6405fd541657d3bb013
Minsu Shin <minsu.shin@navercorp.com>
2021-02-08 11:11:56 +0900

    Fix camera open failure

--- libuvccamera/src/main/jni/libusb/android/jni/libusb.mk
+++ libuvccamera/src/main/jni/libusb/android/jni/libusb.mk
@@ -56,7 +56,9 @@ LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%)
 LOCAL_CFLAGS += -DANDROID_NDK
 LOCAL_CFLAGS += -DLOG_NDEBUG
 LOCAL_CFLAGS += -DACCESS_RAW_DESCRIPTORS
-LOCAL_CFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays
+# https://github.com/saki4510t/UVCCamera/issues/181#issuecomment-335543365
+# LOCAL_CFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays
+LOCAL_CFLAGS += -O1 -fstrict-aliasing -fprefetch-loop-arrays
 LOCAL_EXPORT_LDLIBS += -llog
 LOCAL_ARM_MODE := arm
 
